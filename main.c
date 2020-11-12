5489c45cd27ea1d2edac8f5e010704ce7d4b180f
geonkim0218 <70580063+geonkim0218@users.noreply.github.com>
2020-11-12 15:36:56 +0900

    submission2

--- main.c
+++ main.c
@@ -4,20 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char *pc;
-	int *pi;
-	double *pd;
 	
-	pc = (char*)10000;
-	pi = (int*)10000;
-	pd = (double*)10000;
+	int i = 10;
+	int *ptr = &i;
 	
-	printf("before : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	printf("i = %d, pi : %p\n", i, ptr);
 	
-	pc++;
-	pi++;
-	pd++;
-	printf("after : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	(*ptr)++;
+	printf("i = %d, pi : %p\n", i, ptr);
+	
+	*ptr++;
+	printf("i = %d, pi : %p\n", i, ptr);
 	
 	return 0;
 }
